6340dfee881a318b75984c2f1d7a6b1c97969020
khoazero123 <khoazero123@gmail.com>
2020-08-13 10:25:21 +0700

    Fix bug task 2

--- task-2.js
+++ task-2.js
@@ -10,13 +10,22 @@ function count_groups(friend_pairs) {
   for (let i = 1; i < formatedArr.length; i++) {
     const [idA, idB] = formatedArr[i];
     let isMatch = false;
+    let indexMatch;
     for (let x = 0; x < newArr.length; x++) {
-      if (newArr[x].includes(idA)) {
-        newArr[x].push(idB);
-        isMatch = true;
-      } else if (newArr[x].includes(idB))  {
-        newArr[x].push(idA);
-        isMatch = true;
+      if (!isMatch) {
+        if (newArr[x] && newArr[x].includes(idA)) {
+          newArr[x].push(idB);
+          isMatch = true;
+          indexMatch = x;
+        } else if (newArr[x] && newArr[x].includes(idB))  {
+          newArr[x].push(idA);
+          isMatch = true;
+          indexMatch = x;
+        }
+      } else {
+        newArr[indexMatch].push(idA);
+        newArr[indexMatch].push(idB);
+        delete newArr[x];
       }
     }
     if (!isMatch) {
@@ -24,16 +33,23 @@ function count_groups(friend_pairs) {
     }
     
   }
-
+  newArr = newArr.filter(i => i.length);
   return newArr.length;
 }
-
-console.log(count_groups(`2
-2
-0 1
-2 1`));
-console.log(count_groups(`2
-2
-0 0
-1 1`));
-
+setTimeout(() => { // for debug inspect
+  console.log(count_groups(`2
+  2
+  0 1
+  2 1`));
+  console.log(count_groups(`2
+  2
+  0 0
+  1 1`));
+  console.log(count_groups(`5
+  2
+  0 0
+  1 1
+  6 5
+  1 6
+  9 8`));
+}, 2000);
